cc6ac5dd2e9927afd4da6a40569e931af938dc4a
patrice <moller5121@gmail.com>
2016-11-30 19:18:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Write a function to say what a number squared equals
+
+//Use a for loop to log the square of every number between 1-16
+
+//Create and append a new div to put these results in the page
+	//var div = document.createElement('div');
+	//document.body.appendChild(div);
+
+//Change the CSS for each div - make the text white and background black
+
+//Make the width and height equal to the square
+
+//Events: http://www.w3schools.com/jsref/dom_obj_event.asp
